e5c89125ecbbabc201ab23a8c66c2b198c81e2dd
Harshil <harshilgoti01@gmail.com>
2026-06-28 23:15:51 -0400

    Add smooth scrolling and fix Experience nav link target

--- client/src/components/ServicesSection.tsx
+++ client/src/components/ServicesSection.tsx
@@ -31,7 +31,7 @@ const SERVICES = [
 export default function ServicesSection() {
   return (
     <section
-      id="services"
+      id="experience"
       className="bg-white
         rounded-t-[40px] sm:rounded-t-[50px] md:rounded-t-[60px]
         px-5 sm:px-8 md:px-10 py-20 sm:py-24 md:py-32"
